e465ee20bac3a165ae342b53fcfb58bfa4ae8de8
gato365 <immanuelwilliams@gato365.com>
2023-01-25 03:36:30 -0800

    Added more infomation into homepage

--- controllers/homePage.js
+++ controllers/homePage.js
@@ -49,8 +49,52 @@ router.get('/login', optionalAuth, (req, res) => {
 });
 
 // Sign up page
-
 router.get('/signup', optionalAuth, (req, res) => {
     res.render('signup');
+});
+
+
+// Log out
+router.get('/logout', auth, (req, res) => {
+    req.session.destroy(() => {
+        res.redirect('/login');
+    });
+});
+
+
+// Dashboard page
+router.get('/dashboard', auth, async (req, res) => {
+    const plainUser = req.user.get({ plain: true });
+
+    const posts = await Post.findAll({
+        where: {
+            author_id: req.user.id,
+        },
+        include: [
+            {
+                model: User,
+                as: 'author',
+                attributes: ['id', 'username'],
+            },
+            {
+                model: Comment,
+                as: 'comments',
+                include: [
+                    {
+                        model: User,
+                        as: 'author',
+                        attributes: ['id', 'username'],
+                    },
+                ],
+            },
+        ]
+    });
+
+    const plainPosts = posts.map(post => post.get({ plain: true }));
+
+    res.render('dashboard', {
+        user: plainUser,
+        posts: plainPosts,
+    });
 });
 
