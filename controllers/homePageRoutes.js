90df71b76db943a90232ce20743c370f8aeda5e5
gato365 <immanuelwilliams@gato365.com>
2023-01-30 19:19:05 -0800

    Comment routes

--- controllers/homePageRoutes.js
+++ controllers/homePageRoutes.js
@@ -8,6 +8,7 @@ const allRouter = new Router();
 // allRouter.use('/', router);
 // allRouter.use('/api', apiRouter);
 
+// Home
 allRouter.get('/', (req, res) => {
     res.render("home", {
         name: "Eman",
@@ -16,7 +17,7 @@ allRouter.get('/', (req, res) => {
     });
 });
 
-
+// Login
 allRouter.get('/login', (req, res) => {
     res.render("login", {
         title: "HERE IS THE LOGIN PAGE",
@@ -24,19 +25,20 @@ allRouter.get('/login', (req, res) => {
     });
 });
 
+// Signup
 allRouter.get('/signup', (req, res) => {
     res.render("signup", {
         title: "HERE IS THE SIGNUP PAGE",
 
     });
 });
-
+// Dashboard
 allRouter.get('/dashboard', (req, res) => {
     res.render("dashboard", {
         title: "HERE IS THE DASHBOARD PAGE",
     });
 });
-
+// Logout
 allRouter.get('/logout', (req, res) => {
     res.render("logout", {
         title: "HERE IS THE LOGOUT PAGE",
